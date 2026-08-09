eceacb5958773841a6d7ba9e81bf76f1c70c69ed
KunalGahukar <kunalgahukar1@gmail.com>
2026-08-09 20:14:58 +0530

    Sign up page ready

--- client/src/pages/SignUpScreen.jsx
+++ client/src/pages/SignUpScreen.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import AuthLayout from '../components/AuthLayout'
 import PlayButton from '../components/PlayButton'
 import PlayInput from '../components/PlayInput'
-import { EyeInvisibleOutlined, KeyOutlined, MailOutlined, PhoneOutlined, UserOutlined } from '@ant-design/icons'
+import { EyeInvisibleOutlined, KeyOutlined, MailOutlined, PhoneOutlined, UserOutlined, FacebookFilled, GoogleOutlined, AppleFilled } from '@ant-design/icons'
 
 const SignUpScreen = () => {
     return (
@@ -35,6 +35,17 @@ const SignUpScreen = () => {
                 </div>
                 <PlayButton title={"Sign-Up"} />
             </form>
+
+            <p className='mb-3 text-[15px] font-medium'>Already Have Account?<span className='text-primary'> Login</span></p>
+
+            <div className='flex flex-col items-center gap-5'>
+                <p className='text-[15px] font-medium'>Other way you sign-in</p>
+                <div className='flex justify-center gap-3'>
+                    <FacebookFilled className='text-5xl' />
+                    <GoogleOutlined className='text-5xl' />
+                    <AppleFilled className='text-5xl' />
+                </div>
+            </div>
         </AuthLayout>
     )
 }
